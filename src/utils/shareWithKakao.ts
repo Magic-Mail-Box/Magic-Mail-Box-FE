876ed68d348e0830a6026641731a0c263d156e8f
yechan <noy3928@naver.com>
2022-05-11 13:52:26 +0900

    feat: add share image

--- src/utils/shareWithKakao.ts
+++ src/utils/shareWithKakao.ts
@@ -1,12 +1,11 @@
-import kakaoInitialize from 'utils/kakaoInitialize';
-
 const shareWithKakao = (question: string, advise: string) => {
   window.Kakao.Link.sendDefault({
     objectType: 'feed', // 메시지 형식 : 피드 타입
     content: {
       title: question,
       description: advise,
-      imageUrl: 'https://picsum.photos/200/300', // 메인으로 보여질 이미지 주소
+      imageUrl:
+        'https://s3.ap-northeast-2.amazonaws.com/bookdiver.net/KakaoTalk_Photo_2022-05-11-13-49-05.png', // 메인으로 보여질 이미지 주소
       link: {
         webUrl: 'https://www.naver.com/',
         mobileWebUrl: 'https://www.naver.com/',
